be163a198a82722e2fd817734974909ef3d3456f
MelissaHoffman09 <melissa.hoffman@bryanuniversity.edu>
2021-01-27 20:26:57 -0800

    week5

---
+++ week5/Submit Events/app.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click", () => {
+    var name = document.getElementById("form2").value,
+    number = document.getElementById("form3").value,
+    email = document.getElementById("form4").value;
+
+    alert("Thank you joining our Rewards Program")
+});
